5a134b4c164b8763c1ed468ce24c9dd5fd820323
bailooon <matheus@bailon.com.br>
2025-05-15 15:11:44 -0300

    Initial commit

---
+++ src/main/java/bailooon/com/github/bradesco_dio/BradescoDioApplication.java
@@ -0,0 +1,13 @@
+package bailooon.com.github.bradesco_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bailooon/com/github/bradesco_dio/BradescoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package bailooon.com.github.bradesco_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
