04d6ed66a3cdb1f5eebcf5d456a297a28dc193c8
bailooon <matheus@bailon.com.br>
2025-05-15 16:57:30 -0300

    criando classes de model

--- src/main/java/bailooon/com/github/bradesco/Application.java
+++ src/main/java/bailooon/com/github/bradesco/Application.java
@@ -1,13 +1,13 @@
-package bailooon.com.github.bradesco_dio;
+package bailooon.com.github.bradesco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BradescoDioApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BradescoDioApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
